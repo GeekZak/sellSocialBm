9dfd657495584896d1903c0606c448c1d45f9d8f
GeekZak <abdoulzakoalio98@gmail.com>
2022-07-18 23:20:21 +0100

    first version

---
+++ assets/js/controlForm.js
@@ -0,0 +1,89 @@
+
+
+function showUserForm() {
+    var adduser = document.getElementById("adduser");
+
+    var userform = document.getElementById("userform");
+    
+    adduser.style.display = "none";
+    userform.style.display = "block";
+}
+
+function closeUserForm() {
+    var adduser = document.getElementById("adduser");
+
+    var userform = document.getElementById("userform");
+    
+    adduser.style.display = "block";
+    userform.style.display = "none";
+}
+
+
+
+//catetorie form
+function showCategorieForm() {
+    var addcategorie = document.getElementById("addcategorie");
+
+    var categorieform = document.getElementById("categorieform");
+    
+    addcategorie.style.display = "none";
+    categorieform.style.display = "block";
+}
+
+function closeCategorieForm() {
+    var addcategorie = document.getElementById("addcategorie");
+
+    var categorieform = document.getElementById("categorieform");
+    
+    addcategorie.style.display = "block";
+    categorieform.style.display = "none";
+}
+
+
+//super Categorie form
+function showSupcategorieForm() {
+    var addSupcategorie = document.getElementById("addSupcategorie");
+
+    var Supcategorieform = document.getElementById("Supcategorieform");
+    
+    addSupcategorie.style.display = "none";
+    Supcategorieform.style.display = "block";
+}
+
+function closeSupcategorieForm() {
+    var addSupcategorie = document.getElementById("addSupcategorie");
+
+    var Supcategorieform = document.getElementById("Supcategorieform");
+    
+    addSupcategorie.style.display = "block";
+    Supcategorieform.style.display = "none";
+}
+
+
+
+
+
+//super Categorie form
+function showProductForm() {
+    var addproduct = document.getElementById("addproduct");
+
+    var productform = document.getElementById("productform");
+    
+    addproduct.style.display = "none";
+    productform.style.display = "block";
+}
+
+function closeProductForm() {
+    var addproduct = document.getElementById("addproduct");
+
+    var productform = document.getElementById("productform");
+    
+    addproduct.style.display = "block";
+    productform.style.display = "none";
+}
+
+
+
+
+
+
